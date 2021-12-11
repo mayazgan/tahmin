73f98e6d97171705e48c44ab9fbc9563a41cafef
mayazgan <86611803+mayazgan@users.noreply.github.com>
2021-12-11 17:18:38 +0300

    Update index2.php

--- index2.php
+++ index2.php
@@ -10,8 +10,8 @@
 <tr><td><h1 style="text-shadow: 3px 3px 4px #000;color: #e3d5b5;font-size: 120px;text-align: center;">Tebrikler Bildinnn!<br><a style="text-decoration: none; color: orange;" href="index.php">Tekrar Oyna</a></h1>
 <div style="margin-top: -25px" align="center"><button style='box-shadow: 3px 3px 4px #000;font-size: 20px;border-radius: 50px;border: none;background-color: #333;color: #eee;cursor: pointer;' id='btnFullscreen' type='button'>Tam Ekran &#x26F6;</button></div>
 </td><td id="myiframe" style="padding-right: 20px">
-<div style="box-shadow: 3px 3px 4px #000;" align="center" id="scroller">
-          <iframe id="myiframe" src="kuran.pdf#page=<?php
+<div style="box-shadow: 3px 3px 4px #000;border-radius: 50px" align="center" id="scroller">
+          <iframe style="border-radius: 50px" id="myiframe" src="kuran.pdf#page=<?php
               echo $_SESSION['tutsayi'];
               session_destroy();
           ?>"></iframe>
